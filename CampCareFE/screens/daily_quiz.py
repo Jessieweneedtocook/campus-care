1f66cc8e37fef128e9841d6c7024288510891c61
jessieweneedtocook <J.Smith73@newcastle.ac.uk>
2024-05-09 13:24:34 +0100

    Instance of DailyQuizScreen() created in on_enter(), update_content() changes the question and answer sets displayed on the screen, advance_quiz() enables the screen to automatically update to the next question when an answer is selected

--- CampCareFE/screens/daily_quiz.py
+++ CampCareFE/screens/daily_quiz.py
@@ -1,9 +1,26 @@
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.button import Button
 from kivy.lang import Builder
+
 
 Builder.load_file('kv/dailyquizscreen.kv')
 
 
 class DailyQuizScreen(Screen):
-    pass
+    def on_enter(self):
+        super(DailyQuizScreen, self).on_enter()  # Ensure the superclass method is called
+        self.update_content()
+
+    def update_content(self):
+        self.ids.answers_container.clear_widgets()
+        question = self.manager.get_current_question()
+        self.ids.question_label.text = question['question']
+        for answer in question['answers']:
+            btn = Button(text=answer)
+            btn.bind(on_release=self.advance_quiz)
+            self.ids.answers_container.add_widget(btn)
+
+    def advance_quiz(self, instance):
+        self.manager.next_question()
+        self.update_content()
