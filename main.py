23a02a145686c704d4b82250dbcf39c92b4a3ba0
cellis1912 <C.Ellis1@newcastle.ac.uk>
2024-05-09 11:20:16 +0100

    connecting to docker database

--- main.py
+++ main.py
@@ -1,40 +1,7 @@
-from flask import Flask, request, redirect, url_for, render_template
-#from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-#db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:team37@localhost/campus_care'
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(128))
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        email = request.form['email']
-        user = User(username=username, email=email, password=password)
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('login'))
-    return render_template('register.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = User.query.filter_by(username=username).first()
-        if user and user.password == password:
-            return redirect(url_for(#add home screen name))
-        else:
-            return 'Invalid username or password'
-    return render_template('login.html')
-
-if __name__ == '__main__':
-    db.create_all()
-    app.run(debug=True)
+db = SQLAlchemy(app)
