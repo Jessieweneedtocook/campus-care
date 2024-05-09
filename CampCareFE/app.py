c750e701afe7558a71ec9741a542968610336f4b
jessieweneedtocook <J.Smith73@newcastle.ac.uk>
2024-05-09 01:10:50 +0100

    Set up screen manager and adding screen widgets in MyApp() class

--- CampCareFE/app.py
+++ CampCareFE/app.py
@@ -1,11 +1,19 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager
+from screens.login import LoginScreen
+from screens.signup import SignupScreen
+from screens.reset_password import ResetPasswordScreen
+from screens.daily_quiz import DailyQuizScreen
+from kivy.core.window import Window
 
 
 class MyApp(App):
     def build(self):
-        return Label(text='Hello, Kivy!')
-
-
-if __name__ == '__main__':
-    MyApp().run()
+        Window.size = (375, 667)
+        self.sm = ScreenManager()
+        self.sm.add_widget(LoginScreen(name='login'))
+        self.sm.add_widget(SignupScreen(name='signup'))
+        self.sm.add_widget(ResetPasswordScreen(name='resetpassword'))
+        self.sm.add_widget(DailyQuizScreen(name='dailyquiz'))
+        self.sm.current = 'login'
+        return self.sm
