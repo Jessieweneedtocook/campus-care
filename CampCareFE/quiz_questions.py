69c2df1fed31fbac4f5520287458530c3eb5feba
jessieweneedtocook <J.Smith73@newcastle.ac.uk>
2024-05-09 13:26:51 +0100

    removed new line

--- CampCareFE/quiz_questions.py
+++ CampCareFE/quiz_questions.py
@@ -1,5 +1,4 @@
 questions = [
     {"question": "Hours spent socialising?", "answers": ["Less than 1", "1-3", "More than 4"]},
     {"question": "Hours spent sleeping", "answers": ["Less than 5", "5-7", "More than 7"]},
-    {"question": "Hours spent touching grass?", "answers": ["Less than 1", "1-3", "More than 4"]}
-]
+    {"question": "Hours spent touching grass?", "answers": ["Less than 1", "1-3", "More than 4"]}]
