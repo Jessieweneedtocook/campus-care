5aaec86c6e5e597bb60fc017488558881784fdea
jessieweneedtocook <J.Smith73@newcastle.ac.uk>
2024-05-09 13:20:59 +0100

    Each quiz screen now built sequentially using next_question() by extracting each question and answer set from quiz_questions.py

--- CampCareFE/app.py
+++ CampCareFE/app.py
@@ -5,6 +5,7 @@ from screens.signup import SignupScreen
 from screens.reset_password import ResetPasswordScreen
 from screens.daily_quiz import DailyQuizScreen
 from kivy.core.window import Window
+from quiz_questions import questions
 
 
 class MyApp(App):
@@ -14,6 +15,18 @@ class MyApp(App):
         self.sm.add_widget(LoginScreen(name='login'))
         self.sm.add_widget(SignupScreen(name='signup'))
         self.sm.add_widget(ResetPasswordScreen(name='resetpassword'))
+
+        self.sm.current_question_index = 0
+        self.sm.get_current_question = lambda: questions[self.sm.current_question_index]
+        self.sm.next_question = self.next_question
         self.sm.add_widget(DailyQuizScreen(name='dailyquiz'))
         self.sm.current = 'login'
         return self.sm
+
+    def next_question(self, instance=None):
+        if self.sm.current_question_index < len(questions) - 1:
+            self.sm.current_question_index += 1
+            self.sm.current = 'dailyquiz'
+        else:
+            self.sm.current = 'login'
+
