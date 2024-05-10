dd8272314a5fcfccc770ccaccddd2697fe8fbfd5
cellis1912 <C.Ellis1@newcastle.ac.uk>
2024-05-11 00:25:26 +0100

    set up database and some tests to make sure everything is working

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM mysql:latest
-
-ENV MYSQL_DATABASE campus_care
-ENV MYSQL_ROOT_PASSWORD team37
-
-ADD schema.sql /docker-entrypoint-initdb.d
-
-EXPOSE 3306
--- schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Users (
-    UserID INT AUTO_INCREMENT PRIMARY KEY,
-    Username VARCHAR(255) NOT NULL,
-    Password VARCHAR(255) NOT NULL,
-    Email VARCHAR(255) NOT NULL,
-    DateOfBirth DATETIME NOT NULL,
-    Role ENUM('User', 'Admin') DEFAULT 'User'
-);
