bfeca53263983011d0d5a35aa9c322ff0239b64e
jessieweneedtocook <J.Smith73@newcastle.ac.uk>
2024-05-13 12:48:04 +0100

    Added all the quiz questions to quiz_questions.py

--- CampCareFE/quiz_questions.py
+++ CampCareFE/quiz_questions.py
@@ -1,4 +1,8 @@
 questions = [
-    {"question": "Hours spent socialising?", "answers": ["Less than 1", "1-3", "More than 4"]},
+    {"question": "Hours spent socialising?", "answers": ["Less than 1", "1-3", "More than 3"]},
     {"question": "Hours spent sleeping", "answers": ["Less than 5", "5-7", "More than 7"]},
-    {"question": "Hours spent touching grass?", "answers": ["Less than 1", "1-3", "More than 4"]}]
+    {"question": "Hours spent touching grass?", "answers": ["Less than 1", "1-3", "More than 3"]},
+    {"question": "Hours spent exercising?", "answers": ["None", "1-3", "More than 3"]},
+    {"question": "Hours spent studying?", "answers": ["Less than 1", "1-5", "More than 5"]},
+    {"question": "Hours spent drinking?", "answers": ["None", "less than 3", "More than 3"]},
+    {"question": "Hours spent doing hobbies?", "answers": ["Less than 1", "1-3", "More than 4"]}]
