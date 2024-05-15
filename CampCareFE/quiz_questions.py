34eb9703e8b1bb44fadef2fb226fdaa2c21056a6
jessieweneedtocook <J.Smith73@newcastle.ac.uk>
2024-05-15 18:51:26 +0100

    reference quiz questions now have and activity id, so they can be filtered easily

--- CampCareFE/quiz_questions.py
+++ CampCareFE/quiz_questions.py
@@ -1,8 +1,11 @@
 questions = [
-    {"question": "Hours spent socialising?", "answers": ["Less than 1", "1-3", "More than 3"]},
-    {"question": "Hours spent sleeping", "answers": ["Less than 5", "5-7", "More than 7"]},
-    {"question": "Hours spent touching grass?", "answers": ["Less than 1", "1-3", "More than 3"]},
-    {"question": "Hours spent exercising?", "answers": ["None", "1-3", "More than 3"]},
-    {"question": "Hours spent studying?", "answers": ["Less than 1", "1-5", "More than 5"]},
-    {"question": "Hours spent drinking?", "answers": ["None", "less than 3", "More than 3"]},
-    {"question": "Hours spent doing hobbies?", "answers": ["Less than 1", "1-3", "More than 4"]}]
+    {"question": "Hours spent socialising?", "answers": ["Less than 1", "1-3", "More than 4"], "activity": "Socialisation"},
+    {"question": "Hours spent sleeping", "answers": ["Less than 5", "5-7", "More than 7"], "activity": "Sleeping"},
+    {"question": "Hours spent touching grass?", "answers": ["Less than 1", "1-3", "More than 4"], "activity": "Outdoor time"},
+    {"question": "Hours spent exercising?", "answers": ["Less than 1", "1-3", "More than 4"], "activity": "Exercise"},
+    {"question": "Hours spent studying?", "answers": ["Less than 1", "1-3", "More than 4"], "activity": "Studying"},
+    {"question": "Hours spent drinking?", "answers": ["Less than 1", "1-3", "More than 4"], "activity": "Drinking"},
+    {"question": "Hours spent doing hobbies?", "answers": ["Less than 1", "1-3", "More than 4"], "activity": "Hobbies"}
+]
+
+
