5c9ca968ea016887b6869de29cc84c577ccd0d36
jessieweneedtocook <J.Smith73@newcastle.ac.uk>
2024-05-15 18:36:30 +0100

    Added functionality in initial_options that enables a user to select their activity preferences (what activities they want the quiz to ask about)

--- CampCareFE/screens/initial_options.py
+++ CampCareFE/screens/initial_options.py
@@ -1,5 +1,33 @@
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+
+
+Builder.load_file('kv/initialoptionsscreen.kv')
 
 
 class InitialOptionsScreen(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super(InitialOptionsScreen, self).__init__(**kwargs)
+        self.selected_activities = []
+
+    def toggle_activity(self, instance):
+        if instance.state == 'down':
+            instance.background_color = [0.1, 0.5, 0.8, 1]
+            if instance.text not in self.selected_activities:
+                self.selected_activities.append(instance.text)
+        else:
+            instance.background_color = [0.8, 0.8, 0.8, 1]
+            if instance.text in self.selected_activities:
+                self.selected_activities.remove(instance.text)
+
+    def get_selected_activities(self):
+        return self.selected_activities
+
+    def on_pre_leave(self):
+        from kivy.app import App
+        app = App.get_running_app()
+        app.selected_activities = self.get_selected_activities()
+
+
+
+
