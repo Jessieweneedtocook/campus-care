4bfa5bc7b3c33e70b8ac8d72e21d6ba42ca97bd2
ImTheGuy456 <harrisonmartin2404@gmail.com>
2024-05-16 21:47:25 +0100

    Form to change the users password

--- users/form.py
+++ users/form.py
@@ -44,3 +44,10 @@ class LoginForm(FlaskForm):
     password = PasswordField(validators=[DataRequired()])
 
     submit = SubmitField()
+
+
+class PasswordForm(FlaskForm):
+    current_password = PasswordField(id='password', validators=[DataRequired()])
+    show_password = BooleanField('Show password', id='check')
+    new_password = PasswordField(validators=[DataRequired(), Length(min=8, max=15, ), password_checker])
+    confirm_new_password = PasswordField(validators=[DataRequired(), EqualTo('new_password')])
