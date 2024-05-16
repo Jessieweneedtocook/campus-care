e336909bf3b3879d65b7fda08a14e9fecbc3a54b
ImTheGuy456 <harrisonmartin2404@gmail.com>
2024-05-16 10:51:33 +0100

    Add initial validation forms design

---
+++ users/form.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, DateField, BooleanField, validators
+from wtforms.validators import Email
+from wtforms.validators import ValidationError
+import re
+from wtforms.validators import Length
+from wtforms.validators import DataRequired
+
+
+class SignupForm(FlaskForm):
+    username = StringField(validators=[DataRequired()])
+    email = StringField(validators=[DataRequired(), Email()])
+    phone = StringField(validators=[DataRequired()])
+    dob = DateField(validators=[DataRequired()])
+    password = PasswordField(validators=[DataRequired(), Length])
+    confirm_password = PasswordField(validators=[DataRequired()])
