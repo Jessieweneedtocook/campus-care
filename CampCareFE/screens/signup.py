5f89a4753e5a079e8725e2fe0deb21e5b1074dd5
ImTheGuy456 <harrisonmartin2404@gmail.com>
2024-05-20 13:41:24 +0100

    import validaitons into signup page

--- CampCareFE/screens/signup.py
+++ CampCareFE/screens/signup.py
@@ -1,7 +1,13 @@
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+import requests
 from kivy.uix.boxlayout import BoxLayout
-from users.form import SignupForm
+from users.form import (username_checker,
+                        password_checker,
+                        phone_checker,
+                        dob_checker,
+                        confirm_password_checker,
+                        email_checker)
 
 Builder.load_file('kv/signupscreen.kv')
 
@@ -18,5 +24,20 @@ class SignupScreen(Screen):
         self.signup_form.dob.data = self.ids.dob_input.text
         self.signup_form.password.data = self.ids.password_input.text
         self.signup_form.confirm_password.data = self.ids.confirm_password_input.text
+
+        url = 'http://localhost:5000/register'
+        data = {
+            'username': self.ids.username_input.text,
+            'email': self.ids.email_input.text,
+            'phone': self.ids.phone_input.text,
+            'dob': self.ids.dob_input.text,
+            'password': self.ids.password_input.text,
+            'confirm_password': self.ids.confirm_password_input.text
+        }
+        response = requests.post(url, json=data)
+        if response.status_code == 201:
+            print('User registered successfully')
+        else:
+            print('Registration failed:', response.json().get('message'))
 
 
