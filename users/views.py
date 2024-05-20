ee91f00689fcc299ff701c68423f7271148d0c1d
ImTheGuy456 <harrisonmartin2404@gmail.com>
2024-05-20 14:06:12 +0100

    add backend register function

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from flask import Blueprint
+import requests
+from flask import Blueprint, jsonify
+
+from backend import app
+from users.form import SignupForm
 
 users_blueprint = Blueprint('users', __name__, template_folder='templates')
+
+@users_blueprint.route('/register', methods=['GET', 'POST'])
+def register():
+    data = requests.json
+
+
