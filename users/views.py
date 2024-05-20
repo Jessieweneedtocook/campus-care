b79241bc6fca9c6cf936bbc7c5c3d9619f638279
ImTheGuy456 <harrisonmartin2404@gmail.com>
2024-05-20 14:47:13 +0100

    add register function data for flask

--- users/views.py
+++ users/views.py
@@ -9,5 +9,18 @@ users_blueprint = Blueprint('users', __name__, template_folder='templates')
 @users_blueprint.route('/register', methods=['GET', 'POST'])
 def register():
     data = requests.json
+    form = SignupForm(data=data)
+
+    if form.validate():
+        username = form.username.data
+        email = form.email.data
+        phone = form.phone.data
+        dob = form.dob.data
+        password = form.password.data
+
+        return jsonify({"message": "User registered successfully"}), 201
+    else:
+        errors = form.errors
+        return jsonify({"message": "Validation failed", "errors": errors}), 400
 
 
