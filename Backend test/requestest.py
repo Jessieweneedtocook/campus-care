7e905b0963927cfb6080aa8c1d0a51d63acad522
ImTheGuy456 <harrisonmartin2404@gmail.com>
2024-05-22 14:17:22 +0100

    add function to test login functionality

--- Backend test/requestest.py
+++ Backend test/requestest.py
@@ -15,4 +15,20 @@ def register():
     else:
         print(response.json())
 
+def login():
+    url = 'http://localhost:5001/api'
+    data = {
+        "action": "login_user",
+        'username': "user1",
+        'password': "password"
+    }
+    response = requests.post(url, json=data)
+    if response.status_code == 200:
+        token = response.json().get('access_token')
+        print(f"Login successful, token: {token}")
+        return token
+    else:
+        print(response.json())
+
+
 register()
