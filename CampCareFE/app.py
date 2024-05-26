698ace8a51e1ec3bf4ba643fe08c2dfa82c90c7a
jessieweneedtocook <J.Smith73@newcastle.ac.uk>
2024-05-26 15:53:50 +0100

    added admin screen to screen manager

--- CampCareFE/app.py
+++ CampCareFE/app.py
@@ -17,6 +17,7 @@ from screens.user_info import UserInfoScreen
 from screens.initial_options import InitialOptionsScreen
 from screens.wellness_help import WellnessHelpScreen
 from screens.options import OptionsScreen
+from screens.admin import AdminScreen
 from kivy.core.window import Window
 from quiz_questions import questions
 
@@ -54,6 +55,7 @@ class MyApp(App):
         self.sm.add_widget(InitialOptionsScreen(name='initialoptions'))
         self.sm.add_widget(WellnessHelpScreen(name='wellnesshelp'))
         self.sm.add_widget(OptionsScreen(name='options'))
+        self.sm.add_widget(AdminScreen(name='admin'))
 
         self.daily_quiz_screen = DailyQuizScreen(name='dailyquiz')
         self.sm.add_widget(self.daily_quiz_screen)
