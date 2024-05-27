8e72aed1234e6164db0534888d45897638c60f57
ImTheGuy456 <harrisonmartin2404@gmail.com>
2024-05-27 02:52:52 +0100

    add change email test function

--- Backend test/requestest.py
+++ Backend test/requestest.py
@@ -59,6 +59,23 @@ def change_password(token, current_password, new_password, confirm_new_password)
         print("Failed to change password:", response.json())
 
 
+def change_email(token, new_email):
+    url = 'http://localhost:5001/api'
+    headers = {'Authorization': f'Bearer {token}'}
+    data = {
+        "action": "change_email",
+        "new_email": new_email
+    }
+    response = requests.post(url, json=data, headers=headers)
+    if response.status_code == 200:
+        print("Email changed successfully:", response.json())
+    else:
+        print("Failed to change email:", response.json())
+
+
+
+
+
 register()
 token = login("password")
 if token:
@@ -66,6 +83,7 @@ if token:
     new_token = login("NewPassword1!")
     if new_token:
         print("Re-login successful with new password, new token:", new_token)
+        change_email(new_token, "newemail@gmail.com")
         logout(new_token)
     else:
         print("Failed to log in with new password")
