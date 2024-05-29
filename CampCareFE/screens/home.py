60e0829d163cd07e26f001cc7920b1a57724dccb
cellis1912 <C.Ellis1@newcastle.ac.uk>
2024-05-29 13:08:22 +0100

    updating graphs on entry to screen

--- CampCareFE/screens/home.py
+++ CampCareFE/screens/home.py
@@ -1,12 +1,19 @@
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from kivy.app import App
+
+from CampCareFE.screens.wellness_progress import WellnessProgressScreen
+
 Builder.load_file('kv/homescreen.kv')
 
 
 class HomeScreen(Screen):
     def on_enter(self):
-        pass  # Your existing code
+        screen = WellnessProgressScreen()
+        data_by_activity_type = screen.get_data_for_period(7)
+        stats_by_activity_type = screen.calculate_stats(data_by_activity_type)
+        screen.plot_stats(stats_by_activity_type)
+        screen.overall_progress()
 
     def go_to_daily_quiz(self):
         if App.get_running_app().daily_quiz_comp():
